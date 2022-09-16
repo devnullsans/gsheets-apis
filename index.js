a874e780981faa5575f3d3783b2e244464f38f9d
Paul S <dedsecdarknet@gmail.com>
2022-09-16 21:31:33 +0530

    timing references added

--- index.js
+++ index.js
@@ -11,21 +11,25 @@ async function doAuth() {
 }
 
 let time = null;
+let delay = new Date();
 
 async function loop() {
-  time = Date.now();
+  time = new Date();
   try {
     const sheetData = await getSpreadSheetValues(spreadsheetId, auth, `${spreadsheetName}!A1:C10`);
     const values = sheetData.data.values;
+    const t1 = new Date();
     const niftyData = await getData();
     const filterData = niftyData.filter(n => values.some(v => (n.expiryDate == v[0] && n.strikePrice == v[1])));
     const cmp = values.map(v => String(filterData.find(f => (f.expiryDate == v[0] && f.strikePrice == v[1]))[v[2]]?.lastPrice));
-    const update = await updateSpreadSheetValues(spreadsheetId, auth, `${spreadsheetName}!D1:D10`, [cmp]);
-    console.log(update.data.updatedRange, update.data.updatedCells);
+    const t2 = new Date();
+    const update = await updateSpreadSheetValues(spreadsheetId, auth, `${spreadsheetName}!D1:E10`, [cmp, [time.toLocaleString('en-GB'), t1.toLocaleString('en-GB'), t2.toLocaleString('en-GB'), delay.toLocaleString('en-GB')]]);
+    // console.log(update.data.updatedRange, update.data.updatedCells);
   } catch (error) {
-    console.error(error.message, error.stack);
+    console.error(error.message);
   } finally {
-    setTimeout(() => loop(), 15e3 - (Date.now() - time));
+    delay = new Date();
+    setTimeout(() => loop(), 15e3 - (delay.getTime() - time.getTime()));
   }
 }
 
