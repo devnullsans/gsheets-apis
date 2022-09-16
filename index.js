e5db36520f54d16da4d3c27e45ec176027c30b6a
Paul S <dedsecdarknet@gmail.com>
2022-09-16 09:22:32 +0530

    figured out update sheet

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { getAuthToken, getSpreadSheet, getSpreadSheetValues } = require('./lib');
+const { getAuthToken, getSpreadSheetValues, updateSpreadSheetValues } = require('./lib');
 
 const spreadsheetId = '1bVSXPwvFqX-1K1wZ0-6ojekB0DfLuNoGa2wGCwLf_cs';
 const spreadsheetName = 'Sheet1';
@@ -7,8 +7,8 @@ async function main() {
   try {
     const auth = await getAuthToken();
     console.log('auth', auth.projectId, auth.email, auth.keyId);
-    const sheet = await getSpreadSheet(spreadsheetId, auth);
-    console.log('sheet', JSON.stringify(sheet.data, null, 2));
+    const update = await updateSpreadSheetValues(spreadsheetId, auth, `${spreadsheetName}!A2:B10`, [[1, 2, 3, 4, 5, 6, 7, 8, 9], ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']]);
+    console.log('update', JSON.stringify(update.data, null, 2));
     const value = await getSpreadSheetValues(spreadsheetId, auth, spreadsheetName);
     console.log('value', JSON.stringify(value.data, null, 2));
   } catch (error) {
--- lib.js
+++ lib.js
@@ -5,7 +5,7 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
 // GCLOUD_PROJECT env needs to be set with your-project-id
 const auth = new google.auth.GoogleAuth({
-  keyFile: join(process.cwd(),'keys.json'),
+  keyFile: join(process.cwd(), 'keys.json'),
   scopes: SCOPES
 });
 
@@ -21,8 +21,20 @@ async function getSpreadSheetValues(spreadsheetId, auth, range) {
   return await sheets.spreadsheets.values.get({ spreadsheetId, auth, range });
 }
 
+async function updateSpreadSheetValues(spreadsheetId, auth, range, values) {
+  return await sheets.spreadsheets.values.update({
+    spreadsheetId, auth, range,
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      range, values,
+      majorDimension: 'COLUMNS'
+    }
+  })
+}
+
 module.exports = {
   getAuthToken,
   getSpreadSheet,
-  getSpreadSheetValues
+  getSpreadSheetValues,
+  updateSpreadSheetValues
 };
